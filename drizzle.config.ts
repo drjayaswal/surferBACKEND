cc8506539f23bf14a0ff12bb728a10f41e7b41cc
Dhruv Ratan Jayaswal <168642649+drjayaswal@users.noreply.github.com>
2025-06-14 01:01:07 +0530

    Add files via upload
    raw files

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "drizzle-kit";
+import "dotenv/config";
+
+// via connection params
+export default defineConfig({
+  dialect: "postgresql",
+  dbCredentials: {
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+    user: process.env.DB_USER || "surfer",
+    password: process.env.DB_PASSWORD || "surfer",
+    database: process.env.DB_NAME || "surfer_db",
+  },
+  schema: ["./src/models/*"],
+});
